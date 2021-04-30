f01d347e1866777adf1baaaaadf8332b6b0189aa
federicobenedetti <fedembenedetti@gmail.com>
2021-04-30 01:29:23 -0300

    tercer commit

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // variables
 const circulo = document.getElementById("circulo")
-const timer = document.getElementById("cronometro")
-
-let cronometroSeconds = 30;
+const timer = document.getElementById("timer")
+const score = document.getElementById("score")
+let timerSeconds = 30;
+let points = 1;
 
 // tamaños de pantalla
 const windowWidth = window.innerWidth;
@@ -10,22 +11,47 @@ const windowHeight = window.innerHeight;
 
 
 
+// Inicio del juego //
+
+posicionRandom()
+
+timerFunction = setInterval(function() { // Timer function
+    if (timerSeconds <= 0) {
+        clearInterval(timerFunction); // freno funcion
+    }
+    timer.innerHTML = timerSeconds--;
+}, 1000);
+
+// // 
+
+
+
 // evento click
-circulo.addEventListener("click", posicionRandom);
+circle.addEventListener("click", () => {
+    posicionRandom();
+    addScore();
+});
 
 
 // ubicacion random del circulo
 function posicionRandom() {  
-    circulo.style.top = Math.random() * (windowHeight - 200) +"px";
-    circulo.style.left = Math.random() * (windowHeight - 300) + "px";
+    circle.style.top = Math.random() * (windowHeight - 250) +"px";
+    circle.style.bottom  = Math.random() * (windowHeight - 250) +"px";
+    circle.style.left = Math.random() * (windowWidth -200) + "px";
+    circle.style.right = Math.random() * (windowWidth -200) + "px";
 }
 
 
-// cronometro function
-const cronometroFunction = setInterval(function() {
-    if (cronometroSeconds <= 0) {
-        clearInterval(cronometroFunction); // freno funcion
-    }
-    timer.innerHTML = cronometroSeconds--;
-}, 1000);
+// sumar score
+function addScore() {
+    console.log(score.innerHTML)
+    score.innerHTML = points++;
+}
+
+
+
+
+
+
+
 
