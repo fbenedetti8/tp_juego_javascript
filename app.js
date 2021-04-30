ba565e18d73538c5bc9a6ef07f8d612f0e06376c
federicobenedetti <fedembenedetti@gmail.com>
2021-04-30 12:41:56 -0300

    mnbjk

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // variables
-const circulo = document.getElementById("circulo")
+const circle = document.getElementById("circle")
 const timer = document.getElementById("timer")
 const score = document.getElementById("score")
 let timerSeconds = 30;
 let points = 1;
+let circuleSize = 1;
 
 // tamaños de pantalla
 const windowWidth = window.innerWidth;
@@ -28,15 +29,36 @@ timerFunction = setInterval(function() { // Timer function
 
 // evento click
 circle.addEventListener("click", () => {
+    changeCirculeSize();
     posicionRandom();
     addScore();
 });
 
 
+// cambiar tamaño del circulo
+function changeCirculeSize() {
+    circuleSize = Math.floor(Math.random() * 3); // random del 0 al 2
+    console.log(circuleSize)
+    switch (circuleSize) {
+        case 2:
+            circle.style.height = "10vh";
+            circle.style.width = "10vh";
+            break;
+        case 1: 
+            circle.style.height = "14vh";
+            circle.style.width = "14vh";
+        case 0:
+            circle.style.height = "20vh";
+            circle.style.width = "20vh";
+            break;
+    }
+}
+
+
 // ubicacion random del circulo
 function posicionRandom() {  
     circle.style.top = Math.random() * (windowHeight - 250) +"px";
-    circle.style.bottom  = Math.random() * (windowHeight - 250) +"px";
+    circle.style.bottom = Math.random() * (windowHeight - 300) +"px";
     circle.style.left = Math.random() * (windowWidth -200) + "px";
     circle.style.right = Math.random() * (windowWidth -200) + "px";
 }
@@ -44,8 +66,8 @@ function posicionRandom() {
 
 // sumar score
 function addScore() {
-    console.log(score.innerHTML)
-    score.innerHTML = points++;
+    points = points + circuleSize + 1
+    score.innerHTML = points;
 }
 
 
