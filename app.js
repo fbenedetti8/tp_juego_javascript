c5fd276a3919503e0863047cdb09b30ddac7d075
federicobenedetti <fedembenedetti@gmail.com>
2021-04-29 21:38:32 -0300

    Segunda parte

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
+// variables
 const circulo = document.getElementById("circulo")
+const timer = document.getElementById("cronometro")
+
+let cronometroSeconds = 30;
 
 // tamaños de pantalla
 const windowWidth = window.innerWidth;
 const windowHeight = window.innerHeight;
 
 
-// configuracion inicial del juego
-function start () {
-    setTimeout(myFunction, 3000);
-}
-
 
 // evento click
 circulo.addEventListener("click", posicionRandom);
 
 
-// ubicacion random
+// ubicacion random del circulo
 function posicionRandom() {  
     circulo.style.top = Math.random() * (windowHeight - 200) +"px";
     circulo.style.left = Math.random() * (windowHeight - 300) + "px";
 }
+
+
+// cronometro function
+const cronometroFunction = setInterval(function() {
+    if (cronometroSeconds <= 0) {
+        clearInterval(cronometroFunction); // freno funcion
+    }
+    timer.innerHTML = cronometroSeconds--;
+}, 1000);
 
