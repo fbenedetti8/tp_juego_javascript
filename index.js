c5d3364c954ef85e59ef4a8bfe4eaa423729aaa8
federicobenedetti <fedembenedetti@gmail.com>
2021-05-02 18:41:25 -0300

    yufuytgu

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const dificult = document.getElementsByName("dificult")
+
+console.log(dificult)
+dificult.forEach(dif => {
+    console.log(dif.value)
+});
+// dificult.addEventListener("click", () => {
+//     console.log(hard.checked)
+//     console.log(easy.checked)
+// });
